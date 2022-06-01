f42d5209c1c5b4d5901b129463b4da5aaf595bd4
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-01 12:12:06 +0530

    Import all header components

--- my-portfolio-website/src/components/header/Header.jsx
+++ my-portfolio-website/src/components/header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import CAT from "./CAT";
+import ME from "../../assets/png1.png"
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Thirosh Madhusha</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
 
-export default Header
+        {/* Add Cv Download Link */}
+        <CAT />
+        <HeaderSocial />
+
+        {/* Add Image */}
+        <div className="me">
+          <img src={ME} alt="me"></img>
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
