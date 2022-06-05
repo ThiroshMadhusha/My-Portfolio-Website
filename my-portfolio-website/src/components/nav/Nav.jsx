fa05d6b96844c49648a356d134ea4c6f2c8eb4c6
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-06 05:02:00 +0530

    Create nav bar and user react hooks for active nav class

--- my-portfolio-website/src/components/nav/Nav.jsx
+++ my-portfolio-website/src/components/nav/Nav.jsx
@@ -7,27 +7,55 @@ import { RiServiceLine } from "react-icons/ri";
 import { AiOutlineProject } from "react-icons/ai";
 import { RiCustomerServiceFill } from "react-icons/ri";
 
-
+// Use react hooks for change nav bar active color
+import { useState } from 'react'
 
 const Nav = () => {
+  // Use react hooks for change nav bar active color
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#home">
+      <a
+        href="#"
+        // Use react hooks for change nav bar active color
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <RiServiceLine />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
         <AiOutlineProject />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <RiCustomerServiceFill />
       </a>
     </nav>
