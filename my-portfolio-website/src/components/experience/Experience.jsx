427a392b79132d0543289f978ea7d62cdac48241
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-06 11:41:09 +0530

    Add frontend development languages

--- my-portfolio-website/src/components/experience/Experience.jsx
+++ my-portfolio-website/src/components/experience/Experience.jsx
@@ -1,21 +1,54 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from "react-icons/bs"
 
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skiils I Have</h5>
       <h2>My Experience</h2>
       <div className="container experience__container">
         <div className="experience__frontend">
-
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Java</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Python</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
         </div>
         <div className="experience__backend">
           
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default Experience
