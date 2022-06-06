6c295f8c4743fef4407bf52e1f467342d37f6ec3
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-06 11:25:12 +0530

    Create experience Section

--- my-portfolio-website/src/components/experience/Experience.jsx
+++ my-portfolio-website/src/components/experience/Experience.jsx
@@ -3,7 +3,18 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skiils I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+
+        </div>
+        <div className="experience__backend">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
