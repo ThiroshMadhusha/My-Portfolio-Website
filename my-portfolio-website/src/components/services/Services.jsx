14feabcfa4d577f285a1cd155d31e8bbe4e5a5fe
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-06 15:39:15 +0530

    Start Services Section

--- my-portfolio-website/src/components/services/Services.jsx
+++ my-portfolio-website/src/components/services/Services.jsx
@@ -1,10 +1,89 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from "react-icons/bi"
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
-  )
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container Services__container">
+        <article className="services">
+          <div className="services__head">
+            <h3>Frontend Development</h3>
+          </div>
+          <ui className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ui>
+        </article>
+        {/* End Frontend */}
+        <article className="services">
+          <div className="services__head">
+            <h3>Backend Development</h3>
+          </div>
+          <ui className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ui>
+        </article>
+        {/* End Backend */}
+        <article className="services">
+          <div className="services__head">
+            <h3>Fulstack Development</h3>
+          </div>
+          <ui className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ui>
+        </article>
+        {/* End Fulstac Development */}
+      </div>
+    </section>
+  );
 }
 
 export default Services
