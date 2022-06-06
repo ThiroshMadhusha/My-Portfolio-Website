6fe5e64df27d31a7014288a64e73a16ad902cac2
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-06 20:37:24 +0530

    Change  header names

--- my-portfolio-website/src/components/services/Services.jsx
+++ my-portfolio-website/src/components/services/Services.jsx
@@ -8,7 +8,7 @@ const Services = () => {
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
-      <div className="container Services__container">
+      <div className="container services__container">
         <article className="services">
           <div className="services__head">
             <h3>Frontend Development</h3>
@@ -56,10 +56,11 @@ const Services = () => {
             </li>
           </ui>
         </article>
+
         {/* End Backend */}
         <article className="services">
           <div className="services__head">
-            <h3>Fulstack Development</h3>
+            <h3>Web Development</h3>
           </div>
           <ui className="service__list">
             <li>
@@ -80,7 +81,7 @@ const Services = () => {
             </li>
           </ui>
         </article>
-        {/* End Fulstac Development */}
+        {/* End Web Development */}
       </div>
     </section>
   );
