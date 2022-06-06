0f3db9b41e6b76238fc3429d05a7180da4336b69
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-06 05:51:20 +0530

    Create about me img page

--- my-portfolio-website/src/components/about/About.jsx
+++ my-portfolio-website/src/components/about/About.jsx
@@ -1,13 +1,30 @@
 // rafce command
 import React from 'react'
 import './about.css'
+import ME from '../../assets/png1.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from "react-icons/fi";
+import { AiOutlineProject } from "react-icons/ai";
+
 
 const About = () => {
   return (
-    <section id='about'>
-      About
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me </h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="AboutImage"></img>
+          </div>
+        </div>
+        <div className="about__content">
+          
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default About
