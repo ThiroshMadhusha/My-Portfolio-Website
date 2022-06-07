63ad0668f10c8fff263d186bb1685be9098a1ca2
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-07 16:15:45 +0530

    Add social media chat link

--- my-portfolio-website/src/components/contact/Contact.jsx
+++ my-portfolio-website/src/components/contact/Contact.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
         <div className="contact__option">
           <article className="contact__option">
+            <AiOutlineMail />
+            <h4>Email</h4>
+            <h5>thiroshmadhusha0520@gmail.com</h5>
+            <a href="mailto:thiroshmadhusha0520@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
 
+          <article className="contact__option">
+            <BsLinkedin />
+            <h4>Linkedin</h4>
+            <h5>Thirosh Madhusha</h5>
+            <a
+              href="https://www.linkedin.com/in/thirosh-madhusha"
+              target="_blank"
+            >
+              Send a Message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>Whatsapp</h4>
+            <h5>+94 775338747</h5>
+            <a href="https://wa.me/+94775338747" target="_blank">
+              Send a Message
+            </a>
           </article>
 
           {/* End of contact option */}
 
-          <form action=''>
-            
-          </form>
+          <form action=""></form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
