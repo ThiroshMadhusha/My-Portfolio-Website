0fe6287b18ba4f4522b2dfc195e0972ae184f0ba
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-07 06:06:23 +0530

    Create Testimonials Section

--- my-portfolio-website/src/components/testimonia/Testimonials.jsx
+++ my-portfolio-website/src/components/testimonia/Testimonials.jsx
@@ -3,7 +3,18 @@ import './testimonia.css'
 
 const Testimonials = () => {
   return (
-    <section id='testimonioals'>Testimonials</section>
+    <section id='testimonioals'>
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="" alt="" />
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
