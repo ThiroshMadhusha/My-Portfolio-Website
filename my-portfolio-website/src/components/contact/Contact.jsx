74a21745e87a69809310a95e85574f31cbe44871
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-07 14:30:32 +0530

    Create contact me page section

--- my-portfolio-website/src/components/contact/Contact.jsx
+++ my-portfolio-website/src/components/contact/Contact.jsx
@@ -3,7 +3,24 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__option">
+          <article className="contact__option">
+
+          </article>
+
+          {/* End of contact option */}
+
+          <form action=''>
+            
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
