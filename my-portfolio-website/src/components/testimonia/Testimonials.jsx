e818a502f76aca43899e5d65f41a51aac4bc904b
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-07 11:54:40 +0530

    complete testimonials jsx section

--- my-portfolio-website/src/components/testimonia/Testimonials.jsx
+++ my-portfolio-website/src/components/testimonia/Testimonials.jsx
@@ -5,63 +5,69 @@ import AVTAR2 from "../../assets/avatar2.jpg";
 import AVTAR3 from "../../assets/avatar3.jpg";
 import AVTAR4 from "../../assets/avatar4.png";
 
+
+// import Swiper core and required modules
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTAR1,
+    name: "Client 1",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id molliti orem ipsum dolor sit amet consectetur orem ipsum dolor sit amet consectetur adipisicing elit. Id mollitiadipisicing elit. Id molliti",
+  },
+  {
+    avatar: AVTAR2,
+    name: "Client 2",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id molliti Lorem ipsum dolororem ipsum dolor sit amet consectetur adipisicing elit. Id molliti sit amet consectetur adipisicing elit. Id molliti",
+  },
+  {
+    avatar: AVTAR3,
+    name: "Client 3",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id molliti Lorem orem ipsum dolor sit amet consectetur adipisicing elit. Id mollitiipsum dolor sit amet consectetur adipisicing elit. Id molliti",
+  },
+  {
+    avatar: AVTAR4,
+    name: "Client 4",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id molliti Loremorem ipsum dolor sit amet consectetur adipisicing elit. Id molliti ipsum dolor sit amet consectetur adipisicing elit. Id molliti",
+  },
+];
+
 const Testimonials = () => {
   return (
     <section id="testimonioals">
       <h5>Review From Clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTAR1} alt="Avatar1" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia
-            porro similique laudantium cupiditate repellat minima voluptatibus
-            sit, corrupti inventore dolorem. Pariatur voluptate error maiores?
-            Fugiat maxime qui libero!
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTAR2} alt="Avatar1" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia
-            porro similique laudantium cupiditate repellat minima voluptatibus
-            sit, corrupti inventore dolorem. Pariatur voluptate error maiores?
-            Fugiat maxime qui libero!
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTAR3} alt="Avatar1" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia
-            porro similique laudantium cupiditate repellat minima voluptatibus
-            sit, corrupti inventore dolorem. Pariatur voluptate error maiores?
-            Fugiat maxime qui libero!
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTAR4} alt="Avatar1" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia
-            porro similique laudantium cupiditate repellat minima voluptatibus
-            sit, corrupti inventore dolorem. Pariatur voluptate error maiores?
-            Fugiat maxime qui libero!
-          </small>
-        </article>
-      </div>
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+  
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar Image" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
